5bbd5fe2c750cda21534388e9e755e5004aca5e6
Alexander Neznaev <Corpsetrup@gmail.com>
2018-04-23 21:22:29 +0200

    igor datebase user filling

--- src/main/java/home81/postgre/Attributes.java
+++ src/main/java/home81/postgre/Attributes.java
@@ -1,5 +1,7 @@
 package home81.postgre;
 
+import java.text.SimpleDateFormat;
+
 public class Attributes {
 
     public static final String CREATE_SCHEMA = "CREATE SCHEMA public;";
@@ -7,5 +9,11 @@ public class Attributes {
     public static final String BASE_NAME = "BookShop";
     public static final String PATH = ".\\src\\main\\java\\home81\\postgre\\sql\\";
     public static final String SELECT_AUTORS = "SELECT * from t_autor;";
+    public static final String SELECT_USER_GROUP = "SELECT * from t_user_group;";
+    public static String[] category = { "Басня", "Гимн", "Водевиль", "Былина", "Инвектива", "Драма", "Легенда",
+            "Мадригал", "Комедия", "Новелла", "Ода", "Мелодрама", "Очерк", "Скетч", "Повесть", "Послание", "Трагедия",
+            "Рассказ", "Романс", "Роман", "Сонет", "Сказка", "Стансы", "Эпопея" };
+    public static SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+    public static String[] groups = { "administrators", "developer", "users" };
     
 }
--- src/main/java/home81/postgre/IgorDataBase.java
+++ src/main/java/home81/postgre/IgorDataBase.java
@@ -2,54 +2,165 @@ package home81.postgre;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 
 import dataBase.postgre.PostgreSQLWork;
 import fileOperation.ReaderFromFile;
+import listCreators.NamesAndWords;
 
 public class IgorDataBase {
 
     private static List<List<String>> books = new ArrayList<>();
-    private static String[] category = { "Басня", "Гимн", "Водевиль", "Былина", "Инвектива", "Драма", "Легенда",
-            "Мадригал", "Комедия", "Новелла", "Ода", "Мелодрама", "Очерк", "Скетч", "Повесть", "Послание", "Трагедия",
-            "Рассказ", "Романс", "Роман", "Сонет", "Сказка", "Стансы", "Эпопея" };
 
     public static void main (String[] args) {
-        /*insertIntoAutor();
-        insertIntoBooks();
-        insertIntoUserGroup();*/
-        insertIntoUsers();
+        List<String> manNickNames = new ArrayList<>();
+        manNickNames.add("addy");
+        manNickNames.add("addy2");
+        manNickNames.add("addy3");
+        List<String> womanNickNames = new ArrayList<>();
+        womanNickNames.add("Sara");
+        
+        int amount = manNickNames.size() + womanNickNames.size();
+        for (int i = 0; i < amount; i++) {
+            boolean repeat = true;
+            String nickname = "";
+            boolean random;
+            do {
+                random = Math.random() < 0.5;
+                if (random && !manNickNames.isEmpty()) {
+                    nickname = nicknameFinder(manNickNames);
+                } else {
+                    if (!womanNickNames.isEmpty()) {
+                        nickname = nicknameFinder(womanNickNames);
+                    } else {
+                        nickname = nicknameFinder(manNickNames);
+                        random = true;
+                    }
+                }
+            } while (repeat);
+            System.out.println(nickname);
+            /*insertIntoAutor();
+            insertIntoBooks();
+            insertIntoUserGroup();
+            insertIntoUsers();*/
+        }
     }
 
     private static void insertIntoUsers () {
-        ReaderFromFile reader = new ReaderFromFile(PostgreSQLWork.class.getClassLoader().getResource("EngLastNames.txt"));
-        List<String> lastNames = reader.readAllAsLIst();
-        
-        List<List<String>> groupName = new ArrayList<>();
+        List<String> lastNames = NamesAndWords.englishLastNames();
+        List<String> manNames = NamesAndWords.englishManNames();
+        List<String> manNickNames = NamesAndWords.englishManNickNames();
+        List<String> womanNames = NamesAndWords.englishWomanNames();
+        List<String> womanNickNames = NamesAndWords.englishWomanNickNames();
+        List<String> cities = NamesAndWords.belorussianCities();
 
+        List<List<String>> group = new ArrayList<>();
         PostgreSQLWork postgre = new PostgreSQLWork(Attributes.BASE_NAME);
-        for (int i = 0; i < 15; i++) {
-            ArrayList<String> toAdd = new ArrayList<String>();
-            toAdd.add(lastNames.get((int) (Math.random() * lastNames.size())));
-            groupName.add(toAdd);
+        int amount = manNickNames.size() + womanNickNames.size();
+
+        for (int i = 0; i < amount; i++) {
+            ArrayList<String> toAdd = fillingUser(lastNames, manNames, manNickNames, womanNames, womanNickNames, cities,
+                    postgre);
+            group.add(toAdd);
         }
-        
-        postgre.insertlistToDataBase(groupName, "t_user");
+
+        postgre.insertlistToDataBase(group, "t_user");
         postgre.close();
         System.out.println("t_user filled");
     }
 
+    private static ArrayList<String> fillingUser (List<String> lastNames, List<String> manNames,
+            List<String> manNickNames, List<String> womanNames, List<String> womanNickNames, List<String> cities,
+            PostgreSQLWork postgre) {
+        ArrayList<String> toAdd = new ArrayList<String>();
+
+        // nickname
+        boolean repeat = true;
+        String nickname = "";
+        boolean random;
+        do {
+            random = Math.random() < 0.5;
+            if (random && !manNickNames.isEmpty()) {
+                nickname = nicknameFinder(manNickNames);
+            } else {
+                if (!womanNickNames.isEmpty()) {
+                    nickname = nicknameFinder(womanNickNames);
+                } else {
+                    nickname = nicknameFinder(manNickNames);
+                    random = true;
+                }
+            }
+        } while (repeat);
+
+        // name
+        if (random) toAdd.add(manNames.get((int) (Math.random() * manNames.size())));
+        else toAdd.add(womanNames.get((int) (Math.random() * womanNames.size())));
+
+        // last name
+        toAdd.add(lastNames.get((int) (Math.random() * lastNames.size())));
+
+        // nickname
+        toAdd.add(nickname);
+
+        // password
+        toAdd.add(randomPassword());
+        
+        // address
+        toAdd.add(cities.get((int) (Math.random() * cities.size())));
+
+        // birth date
+        toAdd.add(randomBirthDate());
+
+        List<List<String>> user_group = postgre.executeCustomQuery(Attributes.SELECT_USER_GROUP);
+        toAdd.add(user_group.get(randomIndex()).get(0));
+        return toAdd;
+    }
+
+    private static String nicknameFinder (List<String> nickNames) {
+        String nickname;
+        int index = (int) (Math.random() * nickNames.size());
+        nickname = nickNames.get(index);
+        nickNames.remove(index);
+        return nickname;
+    }
+
+    private static int randomIndex () {
+        double random = Math.random();
+        if (random > 0.9) return 1;
+        if (random < 0.2) return 0;
+        else return 2;
+    }
+
+    private static String randomBirthDate () {
+        long mills = (long) (Math.random() * (Math.random() < 0.5 ? 1 : -0.2) * 1262296800000L);
+        Date date = new Date(mills);
+        String format = Attributes.dateFormat.format(date);
+        return format;
+    }
+
+    private static String randomPassword () {
+        StringBuilder password = new StringBuilder();
+        for (int i = 0; i < (int) (Math.random() * 5 + 10); i++) {
+            String toAppend = "";
+            double random = Math.random();
+            if (random < 0.34) toAppend = (char) (Math.random() * 26 + 'A') + "";
+            else if (random > 0.33 && random < 0.67) toAppend = (char) (Math.random() * 26 + 'a') + "";
+            else toAppend = (char) (Math.random() * 10 + '0') + "";
+            password.append(toAppend);
+        }
+        return password.toString();
+    }
+
     private static void insertIntoUserGroup () {
-        ReaderFromFile reader = new ReaderFromFile(PostgreSQLWork.class.getClassLoader().getResource("EngWoman.txt"));
-        List<String> names = reader.readAllAsLIst();
         List<List<String>> groupName = new ArrayList<>();
 
         PostgreSQLWork postgre = new PostgreSQLWork(Attributes.BASE_NAME);
-        for (int i = 0; i < 15; i++) {
+        for (int i = 0; i < Attributes.groups.length; i++) {
             ArrayList<String> toAdd = new ArrayList<String>();
-            toAdd.add(names.get((int) (Math.random() * names.size())));
+            toAdd.add(Attributes.groups[i]);
             groupName.add(toAdd);
         }
         postgre.insertlistToDataBase(groupName, "t_user_group");
@@ -89,9 +200,7 @@ public class IgorDataBase {
 
         for (List<String> book : books) {
             List<List<String>> autors = postgre.executeCustomQuery(Attributes.SELECT_AUTORS);
-            List<String> currentBook = new ArrayList<>();
-
-            filling(book, autors, currentBook);
+            List<String> currentBook = fillingBook(book, autors);
             booksToQuery.add(currentBook);
         }
         postgre.insertlistToDataBase(booksToQuery, "t_book");
@@ -99,7 +208,8 @@ public class IgorDataBase {
         System.out.println("t_book filled");
     }
 
-    private static void filling (List<String> book, List<List<String>> autors, List<String> currentBook) {
+    private static List<String> fillingBook (List<String> book, List<List<String>> autors) {
+        List<String> currentBook = new ArrayList<>();
         // title
         currentBook.add(book.get(2));
 
@@ -110,7 +220,7 @@ public class IgorDataBase {
         if (book.size() == 4) currentBook.add(book.get(3));
         else currentBook.add("");
 
-        String categ = category[(int) (Math.random() * category.length)];
+        String categ = Attributes.category[(int) (Math.random() * Attributes.category.length)];
         currentBook.add(categ);
 
         String price = (int) ((Math.random() * 200 + 10) * 100) / 100.0 + "";
@@ -118,6 +228,7 @@ public class IgorDataBase {
 
         String amount = (int) (Math.random() * 990 + 10) + "";
         currentBook.add(amount);
+        return currentBook;
     }
 
     private static String findId (String string, List<List<String>> autors) {
